969078ca2aa49cec5e2eac700e7f344e6d20d70e
Abdelhakeem Osama <abdelhakeem.osama@hotmail.com>
2018-04-25 05:34:40 +0200

    Implement HighLevelHashTable

--- include/cs223/HighLevelHashTable.h
+++ include/cs223/HighLevelHashTable.h
@@ -1,19 +1,20 @@
 #ifndef CS223_HIGHLEVELHASHTABLE_H
 #define CS223_HIGHLEVELHASHTABLE_H
 
-#include "HashTable.h"
+#include "LowLevelHashTable.h"
 
 namespace cs223 {
     class HighLevelHashTable : public HashTable {
     public:
-        HighLevelHashTable(UniversalHashFamily &uhf);
+        HighLevelHashTable(UniversalHashFamily &uhf, int size);
 
         bool exists(int key) override;
         void deleteKey(int key) override;
         bool insertKey(int key) override;
+    private:
+        LowLevelHashTable** data;
+        static size_t getSizeBits(int size);
     };
 }
 
 #endif
-
-public:
---
+++ src/HighLevelHashTable.cpp
@@ -0,0 +1,38 @@
+#include <utility>
+#include "HighLevelHashTable.h"
+
+namespace cs223 {
+    HighLevelHashTable::HighLevelHashTable(UniversalHashFamily& uhf, int size)
+            : HashTable(uhf, size) {
+        data = new LowLevelHashTable* [size];
+        for (int i = 0; i < size; i++) {
+            data[i] = new LowLevelHashTable(
+                    UniversalHashFamily(getSizeBits(size), sizeof(int)),
+                    (int) pow(size, 2));
+        }
+    }
+
+    size_t HighLevelHashTable::getSizeBits(int size) {
+        size_t sizeBits = 1;
+        while (size >> 1) {
+            sizeBits += 1;
+        }
+        return sizeBits;
+    }
+
+    bool HighLevelHashTable::insertKey(int key) {
+        int index = hasher->hash(key);
+        return data[index]->insertKey(key);
+    }
+
+    bool HighLevelHashTable::exists(int key) {
+        int index = hasher->hash(key);
+        return data[index]->exists(key);
+    }
+
+    void HighLevelHashTable::deleteKey(int key) {
+        int index = hasher->hash(key);
+        data[index]->deleteKey(key);
+    }
+
+}
