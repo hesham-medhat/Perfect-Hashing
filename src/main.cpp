6265bfccd60502d1f4600af15c12a47ef4490502
Abdelhakeem Osama <abdelhakeem.osama@hotmail.com>
2018-04-25 06:51:49 +0200

    Edit main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,35 +9,43 @@
 
 void checkTable(cs223::LowLevelHashTable table) {
     for (int i = 0; i < table.size; i++) {
-        std::cout << i << (table.exists(i) ? " exists" : " doesn't exist") << std::endl;
+        std::cout << i << (table.existsAtIndex(i) ? " exists" : " doesn't exist") << std::endl;
     }
 }
 
 int main() {
-    //TODO: main function implementation.
+    // TODO main function implementation.
 
-    //TESTING LowLevelHashTable
+    // TESTING LowLevelHashTable
 
-    cs223::UniversalHashFamily hasher(1, 1);
+    cs223::UniversalHashFamily hasher(1, sizeof(int)*8);
     cs223::LowLevelHashTable table(hasher, 2);
+
+    // std::cout << 13 << " hashed to " << hasher.hash(13) << std::endl;
     table.insertKey(13);
+
+    // std::cout << 5 << " hashed to " << hasher.hash(5) << std::endl;
     table.insertKey(5);
+
     checkTable(table);
 
+    // std::cout << 1000 << " hashed to " << hasher.hash(1000) << std::endl;
+    std::cout << "Inserting 1000" << std::endl;
     if (table.insertKey(1000)) {
-        std::cout << "collision at 1000!" << std::endl;
+        // std::cout << "collision at 1000!" << std::endl;
     }
 
+    // std::cout << 57 << " hashed to " << hasher.hash(57) << std::endl;
     if (table.insertKey(57)) {
-        std::cout << "collision at 57!" << std::endl;
+        // std::cout << "collision at 57!" << std::endl;
     }
+
+    // std::cout << 37 << " hashed to " << hasher.hash(37) << std::endl;
     if (table.insertKey(37)) {
-        std::cout << "collision at 37!" << std::endl;
+        // std::cout << "collision at 37!" << std::endl;
     }
 
     checkTable(table);
-
-
 
     return 0;
 }
